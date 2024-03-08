4faca38cbe29df30a3cc13ba4ecc0a3d47f73b55
Emma Packard <emma@packard.co.uk>
2024-03-08 18:04:30 +0000

    making a template
    making a template

--- scripts/eyes_8_140_2019.R
+++ scripts/eyes_8_140_2019.R
@@ -51,8 +51,52 @@ class(eyes_8_140_2019_f$Date) # changing the data format to POSIXct class
 
 #👀🐤 Latency ----
 
-#extracting the first occurence of each RFID tag
-latency<-eyes_8_140_2019_f %>% 
+#first need to filter according to experiment time and last RFID tag before experiment start. Can do this my row number but also time may be better. Look at referencetag dataframe for time values. 
+
+
+#⏰  45mins----
+
+
+eyes_8_2019_t<-subset(eyes_8_140_2019_f,Date >= as.POSIXct('2019-05-05 10:17:40', tz="UTC")) # do from last indicator
+
+#this time is 45 minutes from the above time manually write this
+eyes_8_2019_t2<-subset(eyes_8_2019_t,Date <= as.POSIXct('2019-05-05 11:02:54', tz="UTC")) # make sure 45 mins
+###FOR EMMA TO ADD THE CODE TO ADD THE NESTBOX AND YEAR TO data frame t2
+
+#create a column 'latency' where the date of each row is subtrated from the first row #this data isn't used but it shows you all latecies relative to the reference tag for every visit . its a good sanity check to make sure dataframe is right 
+latency_eyes_8_2019<-eyes_8_2019_t2%>%
+  mutate(latency = Date - Date[row_number()==1])
+
+###here you could just eyeball the row that is the first visit for each tag, extrat that data and save it. 
+#the next code in theory should extract just the first visit for each tag and you could save that df without having to filter by row. 
+
+#extract the latency of the first visit from the reference tag for each tag from the t2 dataframe. please double check the final df matches the one above for latency PER TAG. cross reference output here with the above df output. latencies should be the same 
+
+latency_final_eyes_8_2019<-eyes_8_2019_t2%>%
+  arrange(Date)%>%
+  mutate(latency = Date - Date[row_number()==1])%>%
+  group_by(RFID)%>%
+  slice(1)%>%
+  ungroup()
+
+##everything past here other than saving your dataframe is redundant. 
+
+###add a column that extracts the time between each visit regardless of tag
+btwvisits<-eyes_8_2019_t2%>%
+  arrange(Date)%>%
+  mutate(BtwVisitInterval = Date - lag(Date, default=first(Date)))
+
+#add a column that extracts the time between each visit according to tag
+wtnvisits<-btwvisits%>%
+  group_by(RFID)%>%
+  arrange(Date)%>%
+  mutate(WtnVisitInterval = Date - lag(Date, default=first(Date)))%>%
+  arrange(RFID)%>%
+  ungroup()
+
+
+#extracting the first occurence of each RFID tag and latency of other tags after reference tag 
+latency<-eyes_8_2019_t2 %>% 
   arrange(Date) %>% 
   group_by(RFID) %>% 
   slice(1) %>% # makes the data set and arrages it
@@ -93,13 +137,13 @@ latency4<-eyes_8_140_2019_f%>%
 eyes_8_140_2019_b<-eyes_8_140_2019_f
  
 
-eyes_8_140_2019_b<-cbind(eyes_8_140_2019_b, nestbox='mw10')
+eyes_8_140_2019_b<-cbind(eyes_8_140_2019_b, nestbox='mw10')# maybe do after as different boxes - change name 
 eyes_8_140_2019_b<-cbind(eyes_8_140_2019_b, year='2019')
 head(eyes_8_140_2019_b)
 
 
 # multiples 👯‍♀️----
-
+# select out each, make new df and select by time 
 
 #column names 
 eyes_8_2019<- eyes_8_140_2019_b[c(1,3, 4, 6),]
@@ -111,11 +155,11 @@ eyes_8_2019_1
 
 
 #column names 
-eyes_140_2019<- eyes_8_140_2019_b[c(1, 3, 4, 6),]
+eyes_140_2019<- eyes_8_140_2019_b[c(1, 3, 4, 6),] #not working 
 head(eyes_140_2019)
 
 #delete multiple rows
-eyes_140_2019_1<- eyes_8_140_2019_b[-c(44,70)] 
+eyes_140_2019_1<- eyes_8_140_2019_b[-c(44,70)] #not working 
 head(eyes_140_2019_1)
 
 
