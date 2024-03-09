893c6ac25bdab2d93ebaeb023b2f95362337ac6a
Emma Packard <emma@packard.co.uk>
2024-03-09 23:44:36 +0000

    adding more

---
+++ scripts/eyes_113_2019.R
@@ -0,0 +1,99 @@
+# 📦 PACKAGES ----
+library(tidyverse) # a range of helpful packages
+library(janitor)# helps to format the data
+library(lubridate)# good dates 
+
+
+#💾 IMPORTING DATA ----
+
+setwd("~/GitHub/great_tits/data/2019 Visit counters/100519_MW113_eyes_MW27_eyes")# setting the working directory
+
+eyes_113_2019<-read.delim(file= "C1252RT.TXT", sep="\t", header=TRUE)#loading in data
+names(eyes_113_2019)
+
+
+#🧽 CLEANING DATA ---- 
+
+eyes_113_2019_f<-eyes_113_2019%>%
+  select(Date, Hmsec, CounterID, TagID_1)
+names(eyes_113_2019_f) # flituring the data
+
+
+names(eyes_113_2019_f)[names(eyes_113_2019_f) == "TagID_1"] <- "RFID" # renaming RFID 
+
+
+uniqueIDs<-unique(eyes_113_2019_f$RFID)
+uniqueIDs #lets you see the different ids in the df
+#""           "TagID_1 "   "0300024FEF" "01103F4609" "0110172636"
+
+
+eyes_113_2019_f<-eyes_113_2019_f[!grepl('TagID_1', eyes_113_2019_f$RFID),]# removing tagid one 
+
+eyes_113_2019_f<-eyes_113_2019_f[!eyes_113_2019_f$RFID=="",]#remove blank rows
+head(eyes_113_2019_f)
+
+# 🌎 DATA EXPLORATION ----
+
+uniqueID<- unique(eyes_113_2019_f$RFID)
+uniqueID #only 3 different individuals
+#"0300030EFF" "01101715AA" "01101701B5" "0419483034" "0110174FC8"
+#"0300024FEF", "0300030EFF" -> is the start and stop indicator 
+
+referencetag<-eyes_113_2019_f[eyes_113_2019_f$RFID == '0300030EFF',]#flituring out 0300030EFF
+referencetag # can see when the indicator was used
+
+
+eyes_113_2019_f<-eyes_113_2019_f%>%
+  mutate(Date=ymd_hms(Date))
+class(eyes_113_2019_f$Date) # changing the data format to POSIXct class
+
+#with year/mw ----
+
+#⏰ 45MINS ----
+
+#first need to filter according to experiment time and last RFID tag before experiment start. Can do this my row number but also time may be better. Look at referencetag dataframe for time values. 
+
+
+eyes_113_2019_t<-subset(eyes_113_2019_f,Date >= as.POSIXct('2019-05-10 09:08:28', tz="UTC")) # do from last indicator
+
+#this time is 45 minutes from the above time manually write this
+eyes_113_2019_t2<-subset(eyes_113_2019_t,Date <= as.POSIXct('2019-05-10 09:55:34', tz="UTC")) # make sure 45 mins
+###FOR EMMA TO ADD THE CODE TO ADD THE NESTBOX AND YEAR TO data frame t2
+
+
+# 📅 DATE ----
+# adding the date and the nest box name to the df
+
+eyes_113_2019_b.3<-eyes_113_2019_t2
+
+eyes_113_2019_b.3<-cbind(eyes_113_2019_b.3, nestbox='mw113') 
+eyes_113_2019_b.3<-cbind(eyes_113_2019_b.3, year='2019')
+head(eyes_113_2019_b.3)
+
+
+#👀 LATENCY ----
+
+#create a column 'latency' where the date of each row is subtrated from the first row #this data isn't used but it shows you all latecies relative to the reference tag for every visit . its a good sanity check to make sure dataframe is right 
+latency_eyes_113_2019<-eyes_113_2019_b.3%>%
+  mutate(latency = Date - Date[row_number()==1])
+
+###here you could just eyeball the row that is the first visit for each tag, extrat that data and save it. 
+#the next code in theory should extract just the first visit for each tag and you could save that df without having to filter by row. 
+
+#extract the latency of the first visit from the reference tag for each tag from the t2 dataframe. please double check the final df matches the one above for latency PER TAG. cross reference output here with the above df output. latencies should be the same 
+
+latency_final_eyes_113_2019<-eyes_113_2019_b.3%>%
+  arrange(Date)%>%
+  mutate(latency = Date - Date[row_number()==1])%>%
+  group_by(RFID)%>%
+  slice(1)%>%
+  ungroup()
+
+head(latency_final_eyes_113_2019)
+glimpse(latency_final_eyes_113_2019)
+
+#💾 SAVING----
+setwd("~/GitHub/great_tits/scripts")
+write.csv(latency_final_eyes_113_2019,file="totalVisits45m_mw113_2019.csv")
+
+##everything past here other than saving your dataframe is redundant. 
