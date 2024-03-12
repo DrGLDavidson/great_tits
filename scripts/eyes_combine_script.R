5bfd696f9adfc8d8b745e307ce1bc1388aba8785
Emma Packard <emma@packard.co.uk>
2024-03-12 12:23:39 +0000

    back up

--- scripts/eyes_combine_script.R
+++ scripts/eyes_combine_script.R
@@ -1,13 +1,57 @@
 library(dplyr)
 
-path<-setwd("~/GitHub/great_tits/scripts")
+path<-setwd("~/GitHub/great_tits/scripts") # setting the path
 
-eye_files<-list.files(path, pattern = "totalVisits", recursive = TRUE)
+eye_files<-list.files(path, pattern = "totalVisits", recursive = TRUE) # finding the comman word
 
-eyes<-lapply(eye_files, read.csv)
+eyes<-lapply(eye_files, read.csv) # reading the file 
 
 eyes_combine<- dplyr::bind_rows(eyes)
 
 
 setwd("~/GitHub/great_tits/scripts")
 write.csv(eyes_combine,file="eyes_combine")
+
+
+repeat_birds<- duplicated(eyes_combine$RFID)
+repeat_birds<- as.data.frame(repeat_birds)
+
+
+eyes_with_repeats <- merge(eyes_combine,repeat_birds,by="row.names", all.x = T) 
+write.csv(eyes_with_repeats,file="eyes_with_repeats") # man check control f
+
+# age ----
+
+
+setwd("~/GitHub/great_tits/data")
+individual_age<-read.csv(file= "IndividualsTable_ageatBreedingSeason.csv", header=TRUE)
+
+names(individual_age)
+
+ias<-individual_age %>% 
+  select(age,sex,Age2019BreedingSeason,Age2020BreedingSeason,Age2021BreedingSeason,pitID)
+
+names(ias)[names(ias) == "pitID"] <- "RFID" # chanign the names 
+names(ias)
+
+eyes_ias <-merge (eyes_with_repeats,ias, by = "RFID", all.x = T)
+write.csv(eyes_ias,file="eyes_ias.csv")
+
+# task 
+# excle man copy dates to age col using fliture (new coloumn age at time of sampling - can fliture by year)
+# na - find why the na
+# 20 have missing data (age at breeding season and add man) = gab does this 
+# sex - gab will look 
+# methods - what i have done with the data - extracting master data base etc 
+# mention issues - cross reference id 
+
+# next time - fledgling 
+
+# nestID 2019 
+# select antenna 2  and number fledged - new df 
+# antenna 2 - rename to RFID
+#break down into year eyes_ias (fliture, 3 files)
+# merge by RFID - merge onto the master df 
+# repeat for antenna 1 
+# repeat for 2020, 2021
+# bind rows 
