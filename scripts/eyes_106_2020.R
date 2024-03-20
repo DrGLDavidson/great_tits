8e8c1cc0d429630cdd49298e9771ef1a693c12e0
Emma Packard <emma@packard.co.uk>
2024-03-20 16:12:41 +0000

    error made in 106

--- scripts/eyes_106_2020.R
+++ scripts/eyes_106_2020.R
@@ -8,45 +8,46 @@ library(lubridate)# good dates
 
 setwd("~/GitHub/great_tits/data/2020 Visit counters/200520_MW106_eyes_MW25_eyes_MW75_eyes")# setting the working directory
 
-eyes_89_2020<-read.delim(file= "C1256RT.TXT", sep="\t", header=TRUE)#loading in data
-names(eyes_89_2020)
+eyes_106_2020<-read.delim(file= "C1256RT.TXT", sep="\t", header=TRUE)#loading in data
+names(eyes_106_2020)
 
 
 #🧽 CLEANING DATA ---- 
 
-eyes_89_2020_f<-eyes_89_2020%>%
+eyes_106_2020_f<-eyes_106_2020%>%
   select(Date, Hmsec, CounterID, TagID_1)
-names(eyes_89_2020_f) # flituring the data
+names(eyes_106_2020_f) # flituring the data
 
 
-names(eyes_89_2020_f)[names(eyes_89_2020_f) == "TagID_1"] <- "RFID" # renaming RFID 
+names(eyes_106_2020_f)[names(eyes_106_2020_f) == "TagID_1"] <- "RFID" # renaming RFID 
 
 
-uniqueIDs<-unique(eyes_89_2020_f$RFID)
+uniqueIDs<-unique(eyes_106_2020_f$RFID)
 uniqueIDs #lets you see the different ids in the df
 #"" "TagID_1 "   "0300030EFF" "011016FA48""0110170C32" "0110170740"
 
-eyes_89_2020_f<-eyes_89_2020_f[!grepl('TagID_1', eyes_89_2020_f$RFID),]# removing tagid one 
 
-eyes_89_2020_f<-eyes_89_2020_f[!eyes_89_2020_f$RFID=="",]#remove blank rows
-head(eyes_89_2020_f)
+eyes_106_2020_f<-eyes_106_2020_f[!grepl('TagID_1', eyes_106_2020_f$RFID),]# removing tagid one 
+
+eyes_106_2020_f<-eyes_106_2020_f[!eyes_106_2020_f$RFID=="",]#remove blank rows
+head(eyes_106_2020_f)
 
 # 🌎 DATA EXPLORATION ----
 
-uniqueID<- unique(eyes_89_2020_f$RFID)
+uniqueID<- unique(eyes_106_2020_f$RFID)
 uniqueID #only 3 different individuals
 #"0300030EFF" "011016FA48" "0110170C32" "0110170740"
 
 #"0300024FEF", "0300030EFF" -> is the start and stop indicator 
 
-referencetag<-eyes_89_2020_f[eyes_89_2020_f$RFID == '0300030EFF',]#flituring out 0300030EFF
+referencetag<-eyes_106_2020_f[eyes_106_2020_f$RFID == '0300030EFF',]#flituring out 0300030EFF
 referencetag # can see when the indicator was used
 # different time written down online 
 
 
-eyes_89_2020_f<-eyes_89_2020_f%>%
+eyes_106_2020_f<-eyes_106_2020_f%>%
   mutate(Date=ymd_hms(Date))
-class(eyes_89_2020_f$Date) # changing the data format to POSIXct class
+class(eyes_106_2020_f$Date) # changing the data format to POSIXct class
 
 #with year/mw ----
 
@@ -55,10 +56,10 @@ class(eyes_89_2020_f$Date) # changing the data format to POSIXct class
 #first need to filter according to experiment time and last RFID tag before experiment start. Can do this my row number but also time may be better. Look at referencetag dataframe for time values. 
 
 
-eyes_89_2020_t<-subset(eyes_89_2020_f,Date >= as.POSIXct('2020-05-20 10:42:23 ', tz="UTC")) # do from last indicator
+eyes_106_2020_t<-subset(eyes_106_2020_f,Date >= as.POSIXct('2020-05-20 08:44:05', tz="UTC")) # do from last indicator
 
 #this time is 45 minutes from the above time manually write this
-eyes_89_2020_t2<-subset(eyes_89_2020_t,Date <= as.POSIXct('2020-05-20 11:20:19', tz="UTC")) # make sure 45 mins
+eyes_106_2020_t2<-subset(eyes_106_2020_t,Date <= as.POSIXct('2020-05-20 09:31:41', tz="UTC")) # make sure 45 mins
 #not exactly 45mins 
 
 # missing the final date 
@@ -66,17 +67,17 @@ eyes_89_2020_t2<-subset(eyes_89_2020_t,Date <= as.POSIXct('2020-05-20 11:20:19',
 # 📅 DATE ----
 # adding the date and the nest box name to the df
 
-eyes_89_2020_b.3<-eyes_89_2020_t2
+eyes_106_2020_b.3<-eyes_106_2020_t2
 
-eyes_89_2020_b.3<-cbind(eyes_89_2020_b.3, nestbox='mw106') 
-eyes_89_2020_b.3<-cbind(eyes_89_2020_b.3, year='2020')
-head(eyes_89_2020_b.3)
+eyes_106_2020_b.3<-cbind(eyes_106_2020_b.3, nestbox='mw106') 
+eyes_106_2020_b.3<-cbind(eyes_106_2020_b.3, year='2020')
+head(eyes_106_2020_b.3)
 
 
 #👀 LATENCY ----
 
 #create a column 'latency' where the date of each row is subtrated from the first row #this data isn't used but it shows you all latecies relative to the reference tag for every visit . its a good sanity check to make sure dataframe is right 
-latency_eyes_89_2020<-eyes_89_2020_b.3%>%
+latency_eyes_106_2020<-eyes_106_2020_b.3%>%
   mutate(latency = Date - Date[row_number()==1])
 
 ###here you could just eyeball the row that is the first visit for each tag, extrat that data and save it. 
@@ -84,19 +85,19 @@ latency_eyes_89_2020<-eyes_89_2020_b.3%>%
 
 #extract the latency of the first visit from the reference tag for each tag from the t2 dataframe. please double check the final df matches the one above for latency PER TAG. cross reference output here with the above df output. latencies should be the same 
 
-latency_final_eyes_89_2020<-eyes_89_2020_b.3%>%
+latency_final_eyes_106_2020<-eyes_106_2020_b.3%>%
   arrange(Date)%>%
   mutate(latency = Date - Date[row_number()==1])%>%
   group_by(RFID)%>%
   slice(1)%>%
   ungroup()
 
-head(latency_final_eyes_89_2020)
-glimpse(latency_final_eyes_89_2020)
+head(latency_final_eyes_106_2020)
+glimpse(latency_final_eyes_106_2020)
 
 #💾 SAVING----
 setwd("~/GitHub/great_tits/scripts")
-write.csv(latency_final_eyes_89_2020,file="totalVisits45m_mw106_2020.csv")
+write.csv(latency_final_eyes_106_2020,file="totalVisits45m_mw106_2020.csv")
 
 ##everything past here other than saving your dataframe is redundant. 
 
